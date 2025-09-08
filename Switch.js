5f7a90f583193ba73e5e9ec3e6a4ba5cde29490b
RadhikaRawol <rawolradhika@gmail.com>
2025-09-08 13:59:58 +0530

    JAVASCRIPT

---
+++ Switch.js
@@ -0,0 +1,17 @@
+var money =400;
+switch (money) {
+    case 100:
+        console.log("Rich");
+        break;
+
+    case 200:
+        console.log("very rich");
+        break;
+
+    case 300:
+        console.log("very very rich");
+        break;
+    default:
+        console.log("neither of this");
+        break;
+}
